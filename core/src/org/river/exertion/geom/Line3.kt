26be6ca311bc49967dbbe4819c5015085eb25046
exertionRiver <exertionriver@gmail.com>
2022-03-06 08:25:10 -0700

    fixing some applyNoise transformation math

--- core/src/org/river/exertion/geom/Line3.kt
+++ core/src/org/river/exertion/geom/Line3.kt
@@ -2,13 +2,8 @@ package org.river.exertion.geom
 
 import com.badlogic.gdx.math.MathUtils.cos
 import com.badlogic.gdx.math.MathUtils.sin
-import com.badlogic.gdx.math.Matrix3
 import com.badlogic.gdx.math.Vector3
 import org.river.exertion.*
-import org.river.exertion.geom.Line.Companion.isQ1
-import org.river.exertion.geom.Line.Companion.isQ2
-import org.river.exertion.geom.Line.Companion.isQ3
-import org.river.exertion.geom.Line3.Companion.anglesBetween
 import kotlin.math.*
 
 class Line3(val first : Vector3, val second: Vector3) {
@@ -69,17 +64,16 @@ class Line3(val first : Vector3, val second: Vector3) {
         //transform plots verified with https://c3d.libretexts.org/CalcPlot3D/index.html
         //circle radius and angle for noise to be applied to axis firstPosition -> secondPosition at secondPosition
         //outputs position for noise
-        fun Pair<Vector3, Vector3>.applyNoise(radius : Float, angle : Float) : Vector3 {
+        fun Pair<Vector3, Vector3>.applyNoise(radius : Float, noiseCircleAngle : Float) : Vector3 {
 
             val angles = Pair(this.first, this.second).anglesBetween()
             val azimuth = angles.first
             val polar = angles.second
 
-            val noiseCircle = Vector3(radius * cos(angle), radius * sin(angle), 0f)
+            val noiseCircle = Vector3(radius * cos(noiseCircleAngle.radians()), radius * sin(noiseCircleAngle.radians()), 0f)
 
             noiseCircle.rotate(Vector3(0f, 1f, 0f), polar)
-            noiseCircle.rotate(Vector3(0f, 0f, 1f), azimuth).apply { this.x = -x; this.y = -y } //not sure why this needs to happen
-            noiseCircle.rotate(Vector3(1f, 0f, 0f), 90f)
+            noiseCircle.rotate(Vector3(0f, 0f, 1f), azimuth)
             noiseCircle.add(this.second)
 
             return noiseCircle
